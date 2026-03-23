66959be4743e0dcb7f9f8d138379722f5012120b
Chandrashekhar G <chandru.ganekal@gmail.com>
2026-03-23 11:22:07 +0530

    first prototype

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "ID",
+  appId: "APP_ID"
+};
+
